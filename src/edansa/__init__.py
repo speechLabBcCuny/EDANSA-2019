0516999a952c7eaf66c39fcb9f133f48331684ce
Enis Berk Coban <enisberk@fb.com>
2022-11-24 14:45:20 -0500

    add missing import

--- src/edansa/__init__.py
+++ src/edansa/__init__.py
@@ -1,3 +1,5 @@
-# import megantaxo.metrics
+# package imports are relative to the root of the project, so this is the same as:
+# from edansa import __version__
+import edansa.metrics
 import edansa.taxoutils
 import edansa.preparedataset
