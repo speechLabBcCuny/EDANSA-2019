5b4f17075ce481103c69670c424c2be5ad02d5f8
Enis Berk Çoban <me@enisberk.com>
2023-05-02 13:21:39 -0400

    bump torch version

--- setup.py
+++ setup.py
@@ -144,7 +144,7 @@ setup(
     # conda_env_snapshot.yml
     install_requires=[
         'PyYAML>=5',
-        'torch==1.12.1',
+        'torch==1.13.1',
         'torchaudio==0.12.1',
         'pytorch-ignite==0.4.10',
         'torchvision==0.13.1',
